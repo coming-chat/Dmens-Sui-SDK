59c60d2ba583825d927f615f0addc32947416e0e
0xhelloweb3 <1245816264@qq.com>
2022-11-16 16:09:45 +0800

    feat: add devnet config

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -6,7 +6,6 @@ export class NetworkConfiguration {
       public fullNodeUrl: string,
       public packageObjectId: string,
       public globalId: string,
-      public poolsDynamicId: string,
       public isMainNet = false
     ) {}
   }
@@ -16,7 +15,6 @@ export class NetworkConfiguration {
      Network.DEVNET,
     '0x1f0d4d3ca884a1a6958fe5ba9dc6d8003d9f7d76',
     '0x92131c160fa0f1b95190a3a7cbfa32d0149ab00f',
-    '0x19465f7b8008aa1443269808840856a3c8b2c119'
   );
   
   export const TESTNET_CONFIG = new NetworkConfiguration(
@@ -24,11 +22,18 @@ export class NetworkConfiguration {
      'https://fullnode.testnet.sui.io:443',
     '0xc654deb390bbdd2ab0cdd935a17ef57351f77386',
     '0xed93ebb193b9cb6ba3c603c8f2ad58a606c1fb4f',
-    '0xd96f8bf63c88801dc8a78d8770b40b0b60a1bb8f'
+  );
+
+  export const DEVNET_CONFIG = new NetworkConfiguration(
+    'testnet',
+     Network.DEVNET,
+    '0xa87667208ef2f1f857876a63d7e35148c0d00d14',
+    '0x9fdf5cd0ec667d5ff59175ce49310522eddd6bda',
   );
 
   export const CONFIGS = {
     mainnet: MAINNET_CONFIG,
-    testnet: TESTNET_CONFIG
+    testnet: TESTNET_CONFIG,
+    devnet: DEVNET_CONFIG
   };
   
