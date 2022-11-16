9c8ea7e7b1b62bf78c407dbf0840c4bd7e463649
0xhelloweb3 <1245816264@qq.com>
2022-11-16 17:02:21 +0800

    feat: build post tweet transaction

--- src/interfaces/TweetModule.ts
+++ src/interfaces/TweetModule.ts
@@ -1,5 +1,19 @@
+import { MoveCallTransaction } from '@mysten/sui.js';
 import { IModule } from '../interfaces/IModule';
 import { SDK } from '../sdk';
+
+export type PostTweetParams = {
+  app_id: number,
+  action: number
+  text: string,
+}
+
+export type PostTweetRefParams = {
+  app_id: number,
+  action: number,
+  text: string,
+  ref_identifier: string,
+}
 
 export class TweetModule implements IModule {
     protected _sdk: SDK;
@@ -11,4 +25,33 @@ export class TweetModule implements IModule {
     constructor(sdk: SDK) {
       this._sdk = sdk;
     } 
+
+    buildPostTweetTransaction(params: PostTweetParams, gasPayment: string): MoveCallTransaction {
+      const packageObjectId = this.sdk.networkOptions.packageObjectId;
+      const txn:MoveCallTransaction = {
+        packageObjectId: packageObjectId,
+        module: 'dmens',
+        function: 'post',
+        arguments: [params.app_id,params.action,params.text],
+        typeArguments: [],
+        gasPayment: gasPayment,
+        gasBudget: 10000,
+      }
+      return txn;
+    }
+
+    buildPostTweetWithRefTransaction(params: PostTweetRefParams, gasPayment: string): MoveCallTransaction {
+      const packageObjectId = this.sdk.networkOptions.packageObjectId;
+      const txn:MoveCallTransaction = {
+        packageObjectId: packageObjectId,
+        module: 'dmens',
+        function: 'post',
+        arguments: [params.app_id,params.action,params.text,params.ref_identifier],
+        typeArguments: [],
+        gasPayment: gasPayment,
+        gasBudget: 10000,
+      }
+      return txn;
+    }
+
 }
