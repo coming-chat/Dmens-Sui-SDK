86e62d753dcef750feb39d89215f48cb623a80d7
0xhelloweb3 <1245816264@qq.com>
2022-11-23 18:23:30 +0800

    feat: remove unuseful file

--- codegen.ts
+++ codegen.ts
@@ -4,6 +4,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 const config: CodegenConfig = {
   overwrite: true,
   schema: "https://graphql.coming.chat/sui-testnet/graphql",
+  documents: "src/**/*.graphql",
   generates: {
     "src/generated/graphql.ts": {
       plugins: ["typescript", "typescript-resolvers"]
