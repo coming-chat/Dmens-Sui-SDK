5c2fb19e3c8a3209e2eeecbbeef2a5e71757be37
0xhelloweb3 <1245816264@qq.com>
2023-01-09 16:02:21 +0800

    feat: add fecth mynft

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,23 +1,28 @@
-import { SuiObjectInfo } from "@mysten/sui.js"
 import { SDK, CONFIGS } from "./main"
 
 describe("Dmens Module", ()=>{
     const sdk = new SDK(CONFIGS.devnet)
 
+    const fetchMyNft = async ()=> {
+        const objectList = await sdk.jsonRpcProvider.getObjectsOwnedByAddress(
+            "0x055c9bb0fcfc922633d9cead1f3c85cbf357e4d6"
+        );
+        const cappyObjects = objectList.filter(object => {
+           return object.type.includes("capy::Capy")
+        });
+      
+        const cappyInfoList = await Promise.all(
+            cappyObjects.map(object => {
+               return sdk.jsonRpcProvider.getObject(object.objectId)
+           })
+        );
+        return cappyInfoList;
+    };
+
     test('get users nft', async()=>{
-        const objectList = await sdk.jsonRpcProvider.getObjectsOwnedByAddress("0x055c9bb0fcfc922633d9cead1f3c85cbf357e4d6")
-        const capyList:SuiObjectInfo[] = [];
-        objectList?.map(object => {
-            if (object.type.includes("capy::Capy")) {
-                capyList.push(object)
-            }
-        })
-        console.log(capyList)
-        for (const cappyItem of capyList) {
-            const objectInfo = await sdk.jsonRpcProvider.getObject(cappyItem.objectId);
-            console.log(objectInfo.details)
-        }
-        expect(typeof capyList).toBe('object');
+        const capyInfoList = await fetchMyNft();
+        console.log(capyInfoList)
+        expect(typeof capyInfoList).toBe('object');
     })
    
 })
